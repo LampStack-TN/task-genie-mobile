187e404a01cd117a3e86f98391dd8d866aa38d65
Gaddour <mannai.abdlkader@gmail.com>
2024-04-22 14:20:21 +0100

    Remove nav header

--- App.tsx
+++ App.tsx
@@ -1,29 +1,29 @@
 // In App.js in a new project
-import * as React from 'react';
-import { View, Text ,Button} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Step1 from './components/tasks/steps/Step1';
-import Step2 from './components/tasks/steps/Step2';
-import Step3 from './components/tasks/steps/Step3';
-import Register from './components/auth/register';
-import TaskDetails from './components/taskDetails/TaskDetails';
+import * as React from "react";
+import { View, Text, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Step1 from "./components/tasks/steps/Step1";
+import Step2 from "./components/tasks/steps/Step2";
+import Step3 from "./components/tasks/steps/Step3";
+import Register from "./components/auth/register";
+import TaskDetails from "./components/taskDetails/TaskDetails";
 
-function HomeScreen({navigation}) {
+function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
       <Button
         title="Go to Step1"
-        onPress={() => navigation.navigate('Step1')}
+        onPress={() => navigation.navigate("Step1")}
       />
-       <Button
+      <Button
         title="Go to Register"
-        onPress={() => navigation.navigate('Register')}
+        onPress={() => navigation.navigate("Register")}
       />
-       <Button
+      <Button
         title="Task Details"
-        onPress={() => navigation.navigate('Task')}
+        onPress={() => navigation.navigate("Task")}
       />
     </View>
   );
@@ -33,8 +33,13 @@ const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer >
-      <Stack.Navigator initialRouteName="Home">
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="Home"
+      >
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Step1" component={Step1} />
         <Stack.Screen name="Step2" component={Step2} />

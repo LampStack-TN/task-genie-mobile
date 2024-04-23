e53996204280e4bb818922777b91db44de396400
ahmedGDI <guedria.guedri@gmail.com>
2024-04-23 14:08:20 +0100

    create TabBar comp for navigate from step to another mechanisme

---
+++ components/editTask/MyTabBar.tsx
@@ -0,0 +1,84 @@
+import { Animated, View, TouchableOpacity } from "react-native";
+
+const MyTabBar = ({ state, descriptors, navigation, position }) => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        backgroundColor: "white",
+        paddingVertical: 10,
+      }}
+    >
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: "tabLongPress",
+            target: route.key,
+          });
+        };
+
+        const inputRange = state.routes.map((_, i) => i);
+        const opacity = position.interpolate({
+          inputRange,
+          outputRange: inputRange.map((i) => (i === index ? 1 : 0)),
+        });
+
+        return (
+          <TouchableOpacity
+            accessibilityRole="button"
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{
+              flex: 1,
+              borderRadius: 18,
+              backgroundColor: isFocused ? "#00BFFF" : "#F0F8FF",
+              paddingVertical: 8,
+              paddingHorizontal: 16,
+              marginHorizontal: 4,
+              justifyContent: "center",
+              alignItems: "center",
+              elevation: isFocused ? 4 : 0,
+            }}
+          >
+            <Animated.Text
+              style={{
+                opacity,
+                color: isFocused ? "white" : "black",
+                fontWeight: "bold",
+              }}
+            >
+              {label}
+            </Animated.Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+};
+
+export default MyTabBar;

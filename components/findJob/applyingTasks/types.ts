f28054229b82047b8fd452d33998a64007ccc82a
selim021 <brahamselim09@gmail.com>
2024-04-23 23:36:23 +0200

    Defined TypeScript Task

---
+++ components/findJob/applyingTasks/types.ts
@@ -0,0 +1,8 @@
+export type Task = {
+    id: string;
+    title: string;
+    description: string;
+    location: string;
+    urgency: string;
+    createdAt: string; // Assuming the date is stored as a string
+  };

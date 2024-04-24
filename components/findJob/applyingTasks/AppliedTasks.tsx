a30bc7fa75d9611e58f18729239331fe010189e6
selim021 <brahamselim09@gmail.com>
2024-04-24 17:01:48 +0200

    fetching applied tasks

--- components/findJob/applyingTasks/AppliedTasks.tsx
+++ components/findJob/applyingTasks/AppliedTasks.tsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { ScrollView, View, Text } from 'react-native';
-import axios from 'axios';
-import { Task } from './types';
-import config from '../../../config';
+import React, { useState, useEffect } from "react";
+import { ScrollView, View, Text,StyleSheet } from "react-native";
+import axios from "axios";
+import { Task } from "./types";
+import { MaterialIcons } from '@expo/vector-icons';
 const AppliedTasks = () => {
   const [appliedTasks, setAppliedTasks] = useState<Task[]>([]);
+
 
   useEffect(() => {
     fetchAppliedTasks();
@@ -12,8 +13,10 @@ const AppliedTasks = () => {
 
   const fetchAppliedTasks = async () => {
     try {
-      const userId = 100010; 
-      const response = await axios.get(`${config.apiUrl}/api/task/app/${userId}`);
+      const userId = 10015;
+      const response = await axios.get(
+        `http://localhost:3000/api/task/app/${userId}`
+      );
       setAppliedTasks(response.data);
     } catch (error) {
       console.error(error);
@@ -22,14 +25,44 @@ const AppliedTasks = () => {
 
   return (
     <ScrollView>
-      {appliedTasks.map((task) => (
-        <View key={task.id}>
-          <Text>{task.title}</Text>
-          <Text>{task.description}</Text>
-        </View>
+      {appliedTasks.map((applied) => (
+        <View key={applied.id} style={styles.taskCard}>
+          {/* still handling ts */}
+          <Text style={styles.title}>{applied.task.title}</Text>   
+          <Text style={styles.description}> {applied.task.description}</Text>
+          <MaterialIcons name="pending" size={24} color="black" />
+          <MaterialIcons name="verified" size={24} color="green" />        </View>
       ))}
     </ScrollView>
+    
   );
 };
+const styles = StyleSheet.create({
+  taskCard: {
+    backgroundColor: "#FFFFFF",
+    borderRadius: 20,
+    padding: 16,
+    marginBottom: 16,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    elevation: 8,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 8,
+    color: "#333",
+
+  },
+  description: {
+    fontSize: 14,
+    marginLeft: 8,
+  }
+});
 
 export default AppliedTasks;

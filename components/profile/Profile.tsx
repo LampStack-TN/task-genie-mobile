4d4b089dd442acf6e24d0d347146dd5c1d8a91c6
Gaddour <mannai.abdlkader@gmail.com>
2024-04-26 09:36:39 +0100

    Profile styling

--- components/profile/Profile.tsx
+++ components/profile/Profile.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text, Image, StyleSheet, StatusBar } from "react-native";
-import { FontAwesome6 } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
 import { MaterialIcons } from "@expo/vector-icons";
 // import { useNavigation } from "@react-navigation/native";
 import Button from "../UI/Button";
@@ -8,26 +8,25 @@ const Profile = ({ profile, Data }) => {
   // const navigation = useNavigation();
   return (
     <View style={styles.container}>
-      <View style={{ flex: 1, flexDirection: "row" }}>
-        <View style={{ flex: 1, padding: 25, borderRadius: 100 }}>
+      <View style={styles.header}>
+        <View style={styles.avatar}>
           <Image
             source={{
               uri: "https://st3.depositphotos.com/9998432/13335/v/450/depositphotos_133351928-stock-illustration-default-placeholder-man-and-woman.jpg",
             }}
-            style={{ width: 140, height: 140, borderRadius: 100 }}
+            style={styles.avatarImg}
           />
         </View>
-        <View style={{ flex: 2, alignItems: "flex-start" }}>
-          <Text style={styles.title}> {Data.fullName}</Text>
-
+        <View style={{ flex: 1, alignItems: "flex-start" }}>
+          <Text style={styles.title}>{Data.fullName}</Text>
           <Text style={styles.subTitle}>{profile.jobTitle}</Text>
           <View style={styles.cityContainer}>
-            <FontAwesome6 name="location-pin" size={22} color="#6e6e6e" />
-            <Text style={{ fontSize: 20 }}>{Data.city}</Text>
+            <Entypo name="location-pin" size={24} color="#6e6e6e" />
+            <Text style={{ fontSize: 20, color: "#4e4e4e" }}>{Data.city}</Text>
           </View>
         </View>
       </View>
-      <View style={{ flex: 2 }}>
+      <View style={{ paddingHorizontal: 20, margin: 5 }}>
         <MaterialIcons
           name="email"
           size={24}
@@ -36,9 +35,9 @@ const Profile = ({ profile, Data }) => {
         />
         <Text style={styles.email}>{Data.email}</Text>
         <Text style={styles.bio}>{profile.bio}</Text>
-      </View>
-      <View style={styles.button}>
-        <Button label={"Edit"} style="outline" callback={() => {}}></Button>
+        <View style={styles.button}>
+          <Button label={"Edit"} style="outline" callback={() => {}}></Button>
+        </View>
       </View>
     </View>
   );
@@ -57,17 +56,15 @@ const styles = StyleSheet.create({
     fontSize: 25,
     fontWeight: "bold",
     color: "#0C3178",
-    right: 50,
-    marginStart: 50,
     marginBottom: 10,
     marginTop: 25,
   },
   subTitle: {
+    textAlign: "right",
     fontSize: 20,
     marginBottom: 10,
-    fontWeight: "400",
-    color: "#e6e6e",
-    paddingLeft: 50,
+    fontWeight: "500",
+    color: "#4e4e4e",
   },
   text: {
     justifyContent: "center",
@@ -78,17 +75,16 @@ const styles = StyleSheet.create({
   },
   bio: {
     fontSize: 18,
-    textAlign: "center",
+    textAlign: "left",
     fontWeight: "400",
     color: "#2e2e2e",
   },
   cityContainer: {
     flexDirection: "row",
-    textAlign: "right",
-    paddingLeft: 50,
-
+    justifyContent: "flex-start",
+    textAlign: "left",
     fontWeight: "400",
-    color: "#e6e6e",
+    color: "#6e6e6e",
   },
   email: {
     fontSize: 18,
@@ -98,10 +94,15 @@ const styles = StyleSheet.create({
     color: "#e6e6e",
   },
   button: {
-    flexDirection: "row",
-
-    paddingHorizontal: 22,
-    left: 250,
-    justifyContent: "space-between",
+    alignSelf: "flex-end",
+    marginTop: 50,
   },
+  avatar: { flex: 1, borderRadius: 70 },
+  avatarImg: {
+    width: 140,
+    height: 140,
+    borderRadius: 100,
+    alignSelf: "center",
+  },
+  header: { flexDirection: "row", paddingVertical: 20 },
 });

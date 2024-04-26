2e62bb0f1a4b25f48a31a41ace814d4cc120bcab
Gaddour <mannai.abdlkader@gmail.com>
2024-04-26 11:37:15 +0100

    Handle user global state and related screen management screen

--- App.tsx
+++ App.tsx
@@ -12,95 +12,102 @@ import RegisterIndex from "./components/auth/register/Index";
 import TaskDetails from "./components/taskDetails/TaskDetails";
 import TaskList from "./components/tasks/list/TaskList";
 import MyBottomTab from "./components/navigation/MyBottomTab";
-import store from "./store";
-import { Provider } from "react-redux";
+import store from "./redux/store/store";
+import { Provider, useDispatch, useSelector } from "react-redux";
 import MyTabs from "./components/editTask/MyTabs";
 import MyTasksPosted from "./components/tasks/MyLists/MyTasksPosted";
 import TaskStepsIndex from "./components/tasks/steps";
 import Login from "./components/auth/Login";
 import NearbyJobsScreen from "./components/findJob/applyingTasks/NearbyJobsScreen";
 import AppliedTasks from "./components/findJob/applyingTasks/AppliedTasks";
-import { useLayoutEffect, useState } from "react";
+import { useLayoutEffect } from "react";
 import { ApiClient } from "./api";
 import ProfileIndex from "./components/profile/ProfileIndex";
 import BottomNav from "./components/UI/BottomNav";
 import Menu from "./components/navigation/Menu";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { setUser } from "./redux/slices/userSlice";
 
 const Stack = createNativeStackNavigator();
 
 function App() {
-  const [user, setUser] = useState(null);
-  const [dummy, setDummy] = useState(false);
+  return (
+    <Provider store={store}>
+      <Main />
+    </Provider>
+  );
+}
 
-  const verifyToken = async () => {
+const Main = () => {
+  const user = useSelector((state: any) => state.user);
+  const dispatch = useDispatch();
+
+  const checkAuthentication = async () => {
     try {
       const { data } = await ApiClient().get("auth/verify-token");
       console.log(data);
-      setUser(data);
+      dispatch(setUser(data));
     } catch (error) {
       console.log(error);
       await AsyncStorage.removeItem("token");
-      alert(error.message);
     }
   };
 
   useLayoutEffect(() => {
-    verifyToken();
-  }, [dummy]);
+    if (!user) {
+      checkAuthentication();
+    }
+  }, [user]);
 
   return (
-    <Provider store={store}>
-      <KeyboardAvoidingView
-        style={{ flex: 1 }}
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-      >
-        <NavigationContainer>
-          {user ? (
-            <SafeAreaView
-              style={{
-                flex: 1,
-                marginTop: StatusBar.currentHeight,
-                backgroundColor: "#fff",
-              }}
-            >
-              <View style={{ flex: 1, backgroundColor: "#fff" }}>
-                <Stack.Navigator
-                  screenOptions={{
-                    headerShown: false,
-                  }}
-                  initialRouteName="Home"
-                >
-                  <Stack.Screen name="Home" component={Menu} />
-                  <Stack.Screen name="Step1" component={TaskStepsIndex} />
-                  <Stack.Screen name="TaskDetails" component={TaskDetails} />
-                  <Stack.Screen name="TaskList" component={TaskList} />
-                  <Stack.Screen name="MyTabs" component={MyTabs} />
-                  <Stack.Screen name="MyBottomTab" component={MyBottomTab} />
-                  <Stack.Screen name="Tasks" component={NearbyJobsScreen} />
-                  <Stack.Screen name="AppliedJobs" component={AppliedTasks} />
-                  <Stack.Screen name="ProfileIndex" component={ProfileIndex} />
-                  <Stack.Screen name="MyTasks" component={MyTasksPosted} />
-                </Stack.Navigator>
-              </View>
-              <BottomNav />
-            </SafeAreaView>
-          ) : (
-            <Stack.Navigator
-              screenOptions={{
-                headerShown: false,
-              }}
-              initialRouteName="login"
-            >
-              <Stack.Screen name="login" component={Login} />
-              <Stack.Screen name="Register" component={RegisterIndex} />
-              <Stack.Screen name="Home" component={Menu} />
-            </Stack.Navigator>
-          )}
-        </NavigationContainer>
-      </KeyboardAvoidingView>
-    </Provider>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <NavigationContainer>
+        {user ? (
+          <SafeAreaView
+            style={{
+              flex: 1,
+              marginTop: StatusBar.currentHeight,
+              backgroundColor: "#fff",
+            }}
+          >
+            <View style={{ flex: 1, backgroundColor: "#fff" }}>
+              <Stack.Navigator
+                screenOptions={{
+                  headerShown: false,
+                }}
+                initialRouteName="Home"
+              >
+                <Stack.Screen name="Home" component={Menu} />
+                <Stack.Screen name="Step1" component={TaskStepsIndex} />
+                <Stack.Screen name="TaskDetails" component={TaskDetails} />
+                <Stack.Screen name="TaskList" component={TaskList} />
+                <Stack.Screen name="MyTabs" component={MyTabs} />
+                <Stack.Screen name="MyBottomTab" component={MyBottomTab} />
+                <Stack.Screen name="Tasks" component={NearbyJobsScreen} />
+                <Stack.Screen name="AppliedJobs" component={AppliedTasks} />
+                <Stack.Screen name="ProfileIndex" component={ProfileIndex} />
+                <Stack.Screen name="MyTasks" component={MyTasksPosted} />
+              </Stack.Navigator>
+            </View>
+            <BottomNav />
+          </SafeAreaView>
+        ) : (
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: false,
+            }}
+            initialRouteName="login"
+          >
+            <Stack.Screen name="login" component={Login} />
+            <Stack.Screen name="Register" component={RegisterIndex} />
+          </Stack.Navigator>
+        )}
+      </NavigationContainer>
+    </KeyboardAvoidingView>
   );
-}
+};
 
 export default App;

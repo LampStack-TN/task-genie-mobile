f9b26f11261b4c9e2e87c21773b222716dfe081a
selim021 <brahamselim09@gmail.com>
2024-04-26 02:13:20 +0200

    condional rendring for status

--- components/findJob/applyingTasks/AppliedTasks.tsx
+++ components/findJob/applyingTasks/AppliedTasks.tsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { ScrollView, View, Text,StyleSheet } from "react-native";
+import { ScrollView, View, Text, StyleSheet } from "react-native";
 import axios from "axios";
 import { Task } from "./types";
-import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialIcons } from "@expo/vector-icons";
 import { ApiClient } from "../../../api";
 const AppliedTasks = () => {
   const [appliedTasks, setAppliedTasks] = useState<Task[]>([]);
-
 
   useEffect(() => {
     fetchAppliedTasks();
@@ -14,7 +13,7 @@ const AppliedTasks = () => {
 
   const fetchAppliedTasks = async () => {
     try {
-      const response = await ApiClient().get("/task/app/");        
+      const response = await ApiClient().get("/task/app/");
       setAppliedTasks(response.data);
     } catch (error) {
       console.error(error);
@@ -26,13 +25,19 @@ const AppliedTasks = () => {
       {appliedTasks.map((applied) => (
         <View key={applied.id} style={styles.taskCard}>
           {/* still handling ts */}
-          <Text style={styles.title}>{applied.task.title}</Text>   
+          <Text style={styles.title}>{applied.task.title}</Text>
           <Text style={styles.description}> {applied.task.description}</Text>
-          <MaterialIcons name="pending" size={24} color="black" />
-          <MaterialIcons name="verified" size={24} color="green" />        </View>
+
+          {applied.status === "Pending" ? (
+            <MaterialIcons name="access-time-filled" size={24} color="black" />
+          ) : applied.status === "Accepted" ? (
+            <MaterialIcons name="verified" size={24} color="green" />
+          ) : applied.status === "Rejected" ? (
+            <MaterialIcons name="cancel" size={24} color="red" />
+          ) : null}
+        </View>
       ))}
     </ScrollView>
-    
   );
 };
 const styles = StyleSheet.create({
@@ -55,12 +60,11 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginBottom: 8,
     color: "#333",
-
   },
   description: {
     fontSize: 14,
     marginLeft: 8,
-  }
+  },
 });
 
 export default AppliedTasks;

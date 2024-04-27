72ad3c3c4bca2e4f271d601766746cac4f8ebbb6
selim021 <brahamselim09@gmail.com>
2024-04-27 14:58:23 +0200

    aded popup for delete task

--- components/tasks/client/userTaskList/UserTaskCard.tsx
+++ components/tasks/client/userTaskList/UserTaskCard.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import {
+  Modal,
   View,
   Text,
   StyleSheet,
@@ -13,6 +14,8 @@ const UserTaskCard: React.FC<{
   handleDelete: (taskId: number) => void;
   navigation: any;
 }> = ({ task, handleDelete, navigation }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <Pressable
       onPress={() => navigation.navigate("TaskDetails", { taskId: task.id })}
@@ -51,7 +54,7 @@ const UserTaskCard: React.FC<{
               alignItems: "center",
             }}
           >
-            <TouchableOpacity onPress={() => handleDelete(task.id)}>
+            <TouchableOpacity onPress={() => setModalVisible(true)}>
               <Text style={styles.deleteText}>Delete</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.editButton}>
@@ -65,12 +68,45 @@ const UserTaskCard: React.FC<{
               </Text>
             </TouchableOpacity>
           </View>
+
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>
+                  Are you sure you want to delete this task?
+                </Text>
+                <View style={styles.buttonRow}>
+                  <TouchableOpacity
+                    style={[styles.button, styles.buttonClose]}
+                    onPress={() => setModalVisible(!modalVisible)}
+                  >
+                    <Text style={styles.textStyle}>No</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={[styles.button, styles.buttonConfirm]}
+                    onPress={() => {
+                      handleDelete(task.id);
+                      setModalVisible(!modalVisible);
+                    }}
+                  >
+                    <Text style={styles.textStyle}>Yes</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </Modal>
         </View>
       )}
     </Pressable>
   );
 };
-
 const styles = StyleSheet.create({
   card: {
     backgroundColor: "#FFF",
@@ -133,6 +169,53 @@ const styles = StyleSheet.create({
     color: "#FFFFFF",
     fontWeight: "bold",
     fontSize: 14,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+    backgroundColor: "rgba(0, 0, 0, 0.5)", 
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  buttonRow: {
+    flexDirection: "row",
+    marginTop: 15,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    marginHorizontal: 10, 
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  buttonConfirm: {
+    backgroundColor: "#F44336",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
 

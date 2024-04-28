884d317a382c0cc3e280f86f7a95eb4542bdbdd1
Gaddour <mannai.abdlkader@gmail.com>
2024-04-28 15:38:53 +0100

    Adjust Home menu

--- components/home/Index.tsx
+++ components/home/Index.tsx
@@ -1,12 +1,6 @@
 import React from "react";
-import {
-  StyleSheet,
-  View,
-  Text,
-  TouchableOpacity,
-  Pressable,
-} from "react-native";
-import { Entypo, MaterialIcons } from "@expo/vector-icons";
+import { StyleSheet, View, Text, Pressable } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { NavigationProp } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -18,36 +12,42 @@ type RootStackParamList = {
   TaskList: String;
   AppliedJobs: String;
   Tasks: String;
-  MyTasks:String
+  MyTasks: String;
 };
 
 const Menu = () => {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
   const dispatch = useDispatch();
 
+  const menu = [
+    { id: 1, title: "Task List", name: "MyTasks", icon: "list" },
+    {
+      id: 2,
+      title: "Applied Tasks",
+      name: "AppliedJobs",
+      icon: "check-circle",
+    },
+    { id: 3, title: "Nearby Jobs", name: "Tasks", icon: "task" },
+  ];
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("MyTasks")}
-      >
-        <Entypo name="list" size={24} color="#4e4e4e" />
-        <Text style={styles.buttonText}>Task List</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("AppliedJobs")}
-      >
-        <MaterialIcons name="check-circle" size={24} color="#4e4e4e" />
-        <Text style={styles.buttonText}>Applied Tasks</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("Tasks")}
-      >
-        <MaterialIcons name="task" size={24} color="#4e4e4e" />
-        <Text style={styles.buttonText}>Nearby Jobs</Text>
-      </TouchableOpacity>
+      {menu.map((item) => (
+        <Pressable key={item.id} onPress={() => navigation.navigate(item.name)}>
+          {({ pressed }) => (
+            <View
+              style={[
+                styles.button,
+                pressed && { backgroundColor: "#F58D6120" },
+              ]}
+            >
+              <MaterialIcons name={item.icon} size={24} color="#0C3178" />
+              <Text style={styles.buttonText}>{item.title}</Text>
+            </View>
+          )}
+        </Pressable>
+      ))}
+
       <Pressable
         style={{
           position: "absolute",
@@ -73,7 +73,7 @@ export default Menu;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
+    alignItems: "stretch",
     justifyContent: "flex-start",
     paddingTop: 20,
     backgroundColor: "#fff",
@@ -84,17 +84,16 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     backgroundColor: "#FFF",
-    paddingVertical: 15,
+    paddingVertical: 26,
     paddingHorizontal: 20,
-    width: "100%",
-    elevation: 3,
+    elevation: 1,
     borderBottomWidth: 1,
     borderBlockColor: "#c5c5c5",
   },
   buttonText: {
     marginLeft: 10,
-    fontSize: 16,
-    fontWeight: "bold",
+    fontSize: 22,
+    fontWeight: "600",
     color: "#2e2e2e",
   },
 });

296c02ee13b44483130ab985c9f298dadea66c96
Gaddour <mannai.abdlkader@gmail.com>
2024-04-28 14:40:50 +0100

    Add logout function to dummy professional interface to allow devs to logout

--- components/navigators/ProNavigator.tsx
+++ components/navigators/ProNavigator.tsx
@@ -1,5 +1,6 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 import React from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const ProNavigator = () => {
   return (
@@ -10,16 +11,43 @@ const ProNavigator = () => {
         alignItems: "center",
       }}
     >
-      <Text
-        style={{
-          color: "#0C3178",
-          fontSize: 40,
-          fontWeight: "700",
-          textAlign: "center",
+      <Pressable
+        onPress={() => {
+          alert("You Are Logged Out!! Rload App");
+          AsyncStorage.removeItem("token");
         }}
       >
-        Professionls... Coming Soon...
-      </Text>
+        <Text
+          style={{
+            color: "#F58D61",
+            fontSize: 40,
+            fontWeight: "700",
+            textAlign: "center",
+          }}
+        >
+          Professionls!..
+        </Text>
+        <Text
+          style={{
+            color: "#0C3178",
+            fontSize: 40,
+            fontWeight: "600",
+            textAlign: "center",
+          }}
+        >
+          Coming Soon...
+        </Text>
+        <Text
+          style={{
+            color: "#4e4e4e",
+            fontSize: 22,
+            fontWeight: "500",
+            textAlign: "center",
+          }}
+        >
+          Click Here And Reload App To Logout.
+        </Text>
+      </Pressable>
     </View>
   );
 };

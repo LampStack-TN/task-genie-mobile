b415b38ed74a49b162eca1dba89b5a093e557330
Gaddour <mannai.abdlkader@gmail.com>
2024-04-29 11:57:07 +0100

    check for token in AsyncStorage before verifying token

--- utils/checkAuthentication.ts
+++ utils/checkAuthentication.ts
@@ -3,12 +3,14 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { setUser } from "../redux/slices/userSlice";
 
 const checkAuthentication = async (setLoading, dispatch) => {
-  try {
-    const { data } = await ApiClient().get("auth/verify-token");
-    dispatch(setUser(data));
-  } catch (error) {
-    console.log(error);
-    await AsyncStorage.removeItem("token");
+  if (await AsyncStorage.getItem("token")) {
+    try {
+      const { data } = await ApiClient().get("auth/verify-token");
+      dispatch(setUser(data));
+    } catch (error) {
+      // console.log(error);
+      await AsyncStorage.removeItem("token");
+    }
   }
   setLoading(false);
 };

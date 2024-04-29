d10a400a3562fbe578b26bf69863aafd79229073
lindamnasri <mnasrilinda20@gmail.com>
2024-04-29 11:55:33 +0100

    passing props to PersonalDetails component

---
+++ components/profile/edit/Tabs.tsx
@@ -0,0 +1,34 @@
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import PersonalDetails from "./PersonalDetails";
+import ProfessionalInfo from "./ProfessionalInfo";
+import Security from "./Security";
+import MyTab from "./MyTab";
+
+const Tab = createMaterialTopTabNavigator();
+
+export default function Tabs({route}) {
+  const { Data,profile } = route.params;
+  return (
+    <Tab.Navigator
+      initialRouteName="PersonalDetails"
+      tabBar={(props) => <MyTab {...props} />}
+    >
+      <Tab.Screen
+        name="PersonalDetails"
+        component={PersonalDetails}
+        initialParams={{ Data: Data, profile: profile }}
+        options={{ tabBarLabel: "Personal Details" }}
+      />
+      <Tab.Screen
+        name="ProfessionalInfo"
+        component={ProfessionalInfo}
+        options={{ tabBarLabel: "Professional Info" }}
+      />
+      <Tab.Screen
+        name="Security"
+        component={Security}
+        options={{ tabBarLabel: "Security" }}
+      />
+    </Tab.Navigator>
+  );
+}

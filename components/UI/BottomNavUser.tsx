51e66e94e287dd63b0fb9209127f7fad5a61a14d
Gaddour <mannai.abdlkader@gmail.com>
2024-04-29 11:29:04 +0100

    implement slide up menu to buttom nav

---
+++ components/UI/BottomNavUser.tsx
@@ -0,0 +1,74 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Entypo } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import SlideUp from "./SlideUp";
+
+const BottomNavUser: React.FC = () => {
+  const navigation = useNavigation();
+  const [slideOn, setSlideOn] = useState(false);
+
+  const toggleSlide = () => setSlideOn(!slideOn);
+
+  const navItems = [
+    { id: 1, screen: "Home", icon: "home" },
+    { id: 2, screen: "Step1", icon: "circle-with-plus" },
+    // { id: 3, screen: "ProfileIndex", icon: "user" },
+  ];
+  return (
+    <View style={styles.navContainer}>
+      {navItems.map(({ id, screen, icon }: any) => (
+        <View key={id} style={styles.navItem}>
+          <Pressable
+            onPress={() => navigation.navigate(screen)}
+            style={styles.navIcon}
+          >
+            {({ pressed }) => (
+              <Entypo
+                name={icon}
+                size={32}
+                color={pressed ? "#0C3178" : "#5275B7"}
+              />
+            )}
+          </Pressable>
+        </View>
+      ))}
+      <View style={styles.navItem}>
+        <Pressable onPress={toggleSlide} style={styles.navIcon}>
+          {({ pressed }) => (
+            <Entypo
+              name="menu"
+              size={32}
+              color={pressed ? "#0C3178" : "#5275B7"}
+            />
+          )}
+        </Pressable>
+        <SlideUp {...{ slideOn, toggleSlide, navigation }} />
+      </View>
+    </View>
+  );
+};
+
+export default BottomNavUser;
+
+const styles = StyleSheet.create({
+  navContainer: {
+    borderTopWidth: 1,
+    borderColor: "#c5c5c5",
+    height: 60,
+    justifyContent: "space-around",
+    flexDirection: "row",
+  },
+  navItem: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  navIcon: {
+    flex: 1,
+    height: "100%",
+    width: 60,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});

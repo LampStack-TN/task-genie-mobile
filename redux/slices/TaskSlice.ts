72c6ed8cb226a9f2a301f291d57f9773e30049d3
lindamnasri <mnasrilinda20@gmail.com>
2024-04-29 18:09:19 +0100

    remove clientId

--- redux/slices/TaskSlice.ts
+++ redux/slices/TaskSlice.ts
@@ -2,12 +2,11 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const taskSlice = createSlice({
   name: "task", // Slice name
-  initialState: { clientId: 10006}, // Initial state of the slice
+  initialState: {}, // Initial state of the slice
   reducers: {
-
     //function to add/update task
     addTask: (state, action) => {
-      return { ...state, ...action.payload };//Merge payload with current state
+      return { ...state, ...action.payload }; //Merge payload with current state
     },
   },
 });

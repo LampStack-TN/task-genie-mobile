5c22b57511c40497006445a40c12b3ecfd6c38a6
lindamnasri <mnasrilinda20@gmail.com>
2024-04-29 10:31:23 +0100

    init PersonalDetails screen

---
+++ components/profile/edit/PersonalDetails.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function PersonalDetails() {
+  return (
+    <View>
+      <Text>PersonalDetails</Text>
+    </View>
+  )
+}

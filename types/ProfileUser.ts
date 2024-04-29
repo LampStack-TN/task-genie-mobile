573cfba889c8cc5613c923860e437261f928ea12
selim021 <brahamselim09@gmail.com>
2024-04-30 12:25:26 +1300

    Define ProfileUser interface with user and profile details

---
+++ types/ProfileUser.ts
@@ -0,0 +1,22 @@
+interface ProfileUser {
+    id: number;
+    fullName: string;
+    email: string;
+    password: string;
+    role: string;
+    phone: string | null;
+    birthdate: string | null;
+    city: string;
+    address: string;
+    zipcode: string;
+    avatar: string;
+    profile: {
+      id: number;
+      jobTitle: string;
+      bio: string;
+      userId: number;
+      
+    };
+  }
+
+  export default ProfileUser

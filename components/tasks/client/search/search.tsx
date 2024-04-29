ef7885911d4b59613e345a78fcb9895479666a9b
ahmedGDI <guedria.guedri@gmail.com>
2024-04-29 18:32:08 +0100

    style button , icon & input in search comp

--- components/tasks/client/search/search.tsx
+++ components/tasks/client/search/search.tsx
@@ -74,16 +74,16 @@ const Search = ({ onSearchResults }:any) => {
                 <Ionicons name="search" size={20} color="#cb6e17"  />
                 <TextInput
                   placeholder="Job, Title, Skill, Expertise..."
-                  
+                  style={styles.input}
                   value={searchQuery}
                   onChangeText={setSearchQuery}
                 />
               </View>
               <View style={[styles.inputContainer, styles.secondInputContainer]}>
-                <Ionicons name="location-sharp" size={20} color="#cb6e17"  />
+                <Ionicons name="location-sharp" size={20} color="#cb6e17" style={styles.icon} />
                 <Picker
                   selectedValue={selectedCity}
-                  
+                  style={styles.picker}
                   onValueChange={(itemValue, itemIndex) => setSelectedCity(itemValue)}
                 >
                   <Picker.Item label="Select a city" value="" />
@@ -92,8 +92,8 @@ const Search = ({ onSearchResults }:any) => {
                   ))}
                 </Picker>
               </View>
-              <TouchableOpacity  onPress={onFindJobsPress}>
-                <Text >Find Jobs</Text>
+              <TouchableOpacity  onPress={onFindJobsPress} style={styles.button}>  
+                <Text style={styles.buttonText}>Find Jobs</Text>
               </TouchableOpacity>
             </View>
           );
@@ -102,19 +102,42 @@ const Search = ({ onSearchResults }:any) => {
 export default Search;
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#ffd0ad',
-    padding: 15,
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#ffe8d6',
-    borderRadius: 20,
-    padding: 10,
-    marginBottom: 10,
-  },
-  secondInputContainer: {
-    marginBottom: 20,
-  }
-});
+    container: {
+      backgroundColor: '#ffd0ad',
+      padding: 15,
+    },
+    inputContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      backgroundColor: '#ffe8d6',
+      borderRadius: 20,
+      padding: 10,
+      marginBottom: 10,
+    },
+    secondInputContainer: {
+      marginBottom: 20,
+    },
+    input: {
+      flex: 1,
+      paddingLeft: 10,
+      color: '#cb6e17',
+    },
+    icon: {
+      marginLeft: 10,
+    },
+    button: {
+      backgroundColor: "#0C3178",
+      borderRadius: 20,
+      paddingVertical: 15,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    buttonText: {
+      color: '#fff',
+      fontWeight: 'bold',
+    },
+    picker: {
+      flex: 1,
+      color: '#cb6e17',
+    },
+  });

b67eb9b91bc038ad57c510b2d672595d347bde2d
lindamnasri <mnasrilinda20@gmail.com>
2024-04-30 13:43:06 +0100

    service card component

---
+++ components/professional/services/ServicesCard.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ServicesCard = () => {
+  return (
+    <View>
+      <Text>ServicesCard</Text>
+    </View>
+  )
+}
+
+export default ServicesCard

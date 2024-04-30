c2860e9980ff8eeffcccb7f966692ce5d17db21e
ahmedGDI <guedria.guedri@gmail.com>
2024-04-30 10:30:47 +0100

    create proDetails comp

---
+++ components/tasks/professional/proDetails/proDetails.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const proDetails = ({navigation,route}) => {
+  console.log("route :" ,route)
+    return (
+    <View>
+      <Text>proDetails</Text>
+    </View>
+  )
+}
+
+export default proDetails
+
+const styles = StyleSheet.create({})

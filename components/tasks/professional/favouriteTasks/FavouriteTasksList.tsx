17bb4083dda8012b22132183aad04e998b4ddd5a
selim021 <brahamselim09@gmail.com>
2024-04-30 17:16:17 +0100

    Add FavouriteTasksList component with toggleLike  and apply functionality

---
+++ components/tasks/professional/favouriteTasks/FavouriteTasksList.tsx
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from "react";
+import { ScrollView, View, Text, StyleSheet } from "react-native";
+import TaskCard from "../applyingTasks/TaskCard";
+import { ApiClient } from "../../../../utils/api";
+import Task from "../../../../types/Task";
+const FavouriteTasksList = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [appliedTasks, setAppliedTasks] = useState<string[]>([]);
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+  useEffect(() => {
+    const fetchLikedTasks = async () => {
+      try {
+        const response = await ApiClient().get("/task/favoriteTasks");
+        setTasks(
+          response.data.map((task) => ({
+            ...task,
+            liked: true,
+          }))
+        );
+      } catch (error) {
+        console.error("Failed to fetch favorite tasks:", error);
+      }
+    };
+
+    fetchLikedTasks();
+  }, []);
+
+  const toggleLikeTask = async (taskId) => {
+    try {
+      const response = await ApiClient().post("/task/likeTask", { taskId });
+      setTasks((currentTasks) =>
+        currentTasks.map((task) => {
+          if (task.id === taskId) {
+            return { ...task, liked: !task.liked };
+          }
+          return task;
+        })
+      );
+    } catch (error) {
+console.log(error);
+
+    }
+  };
+
+  const handleApplyToTask = async (appliedTask: Task) => {
+    try {
+      const response = await ApiClient().post("/task/apply", {
+        taskId: appliedTask.id,
+      });
+      console.log(response.data);
+      //remove the applied task from state
+      setTasks(tasks.filter((task) => task.id !== appliedTask.id));
+      // Update appliedTasks state to add the appliedtask.id
+      setAppliedTasks([...appliedTasks, appliedTask.id]);
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setModalMessage(error.response.data.message);
+        setModalVisible(true);
+      } else {
+        console.error(error);
+      }
+    }
+  };
+
+  return (
+    <ScrollView style={styles.container}>
+      {tasks.length > 0 ? (
+        tasks.map((task) => (
+          <TaskCard
+            key={task.id}
+            task={task}
+            onToggleLike={() => toggleLikeTask(task.id)}
+            onApply={() => handleApplyToTask(task)}
+          />
+        ))
+      ) : (
+        <Text>No favorite tasks found.</Text>
+      )}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default FavouriteTasksList;

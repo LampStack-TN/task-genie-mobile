5ac984623c4ee7d96bb3ba2331e369e3ebe4a60b
lindamnasri <mnasrilinda20@gmail.com>
2024-04-30 20:53:25 +0100

    init ServiceDetails component

---
+++ components/professional/serviceDetails/ServiceDetails.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ServiceDetails = () => {
+  return (
+    <View>
+      <Text>ServiceDetails</Text>
+    </View>
+  )
+}
+
+export default ServiceDetails

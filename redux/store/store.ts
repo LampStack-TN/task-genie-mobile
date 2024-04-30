dbf706708b71c0d7e254b1ca105ce9745428695b
lindamnasri <mnasrilinda20@gmail.com>
2024-04-30 19:49:00 +0100

    store the serviceReducer

--- redux/store/store.ts
+++ redux/store/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import taskReducer from "../slices/TaskSlice";
 import registerReducer from "../slices/registerSlice";
 import userReducer from "../slices/userSlice";
+import serviceReducer from "../slices/serviceSlice";
 
 export default configureStore({
   reducer: {
     task: taskReducer,
     registerData: registerReducer,
     user: userReducer,
+    service: serviceReducer,
   },
 });

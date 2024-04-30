ee2d2af1535cf48e8e61a494d4921a34a0434da4
lindamnasri <mnasrilinda20@gmail.com>
2024-04-30 19:44:12 +0100

    init components of form services

---
+++ components/professional/form/Price.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Price = () => {
+  return (
+    <View>
+      <Text>Price</Text>
+    </View>
+  )
+}
+
+export default Price
---
+++ components/professional/form/Skills.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <View>
+      <Text>Skills</Text>
+    </View>
+  )
+}
+
+export default Skills

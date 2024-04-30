4f34d8ae95a727e90624e552f3ed0bf06daeeb68
lindamnasri <mnasrilinda20@gmail.com>
2024-04-30 13:42:54 +0100

    servecies list component

---
+++ components/professional/services/ServicesList.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+
+
+const ServicesList = () => {
+  return (
+    <View>
+      <Text>ServicesList</Text>
+    </View>
+  )
+}
+
+export default ServicesList

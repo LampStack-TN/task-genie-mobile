678d0e99f6580b6960349082ee288701ebcb0028
ahmedGDI <guedria.guedri@gmail.com>
2024-05-01 19:21:43 +0100

    handle the pickImage using ImagePicker, working with an image as a mediaType, and setValue to defaultValue (avatar)

--- components/auth/register/BasicInfos.tsx
+++ components/auth/register/BasicInfos.tsx
@@ -27,9 +27,23 @@ const BasicInfos = ({ navigation }:any) => {
       avatar : null
     },
   });
+  
   const onSubmit = (data) => {
-    dispatch(appendData(data));
+    dispatch(appendData({ ...data }));
     navigation.navigate("contact");
+  };
+
+  const handlePickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    
+    if (result) {
+      setValue("avatar", result.assets[0].uri); 
+    }
   };
   return (
     <View style={styles.container}>

b1b663aa79a4c8b5ed11360333bb16afb4c8ef9e
Gaddour <mannai.abdlkader@gmail.com>
2024-05-04 11:18:21 +0100

    add baseUrl to config

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 const config = {
-    apiUrl: "http://192.168.15.54:3001/api",
-  };
-  
-  export default config;
-  
+  apiUrl: "http://192.168.15.54:3001/api",
+  baseUrl: "http://192.168.15.54:3001",
+};
+
+export default config;

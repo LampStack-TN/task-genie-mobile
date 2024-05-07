d6dce9c79363c77b07db0f2cc214f6d7e0e9507f
lindamnasri <mnasrilinda20@gmail.com>
2024-05-07 13:24:34 +0100

    add documents component

---
+++ components/professional/profile/Documents.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Documents = () => {
+  return (
+    <View>
+      <Text>Documents</Text>
+    </View>
+  )
+}
+
+export default Documents

f3c0680584800d954e4d78cdf1245d02d7e00de1
lindamnasri <mnasrilinda20@gmail.com>
2024-05-15 20:45:33 +0100

    adjust style applied task

--- screens/professional/AppliedTasks.tsx
+++ screens/professional/AppliedTasks.tsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { ScrollView, View, Text, StyleSheet } from "react-native";
+import { ScrollView, View, Text, StyleSheet, Pressable, FlatList } from "react-native";
 import Application from "../../types/Application";
 import { MaterialIcons } from "@expo/vector-icons";
 import { ApiClient } from "../../utils/api";
-const AppliedTasks = () => {
-  const [applications, seApplication] = useState<Application[]>([]);
+import Icon from "react-native-vector-icons/MaterialIcons";
+import TaskCard from "../../components/professional/TaskCard";
+const AppliedTasks = (navigation) => {
+  const [applications, setApplication] = useState<Application[]>([]);
 
   useEffect(() => {
     fetchAppliedTasks();
@@ -13,49 +15,44 @@ const AppliedTasks = () => {
   const fetchAppliedTasks = async () => {
     try {
       const response = await ApiClient().get("/task-application");
-      seApplication(response.data);
+      setApplication(response.data);
+      // console.log(response.data);
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    <ScrollView>
+    <ScrollView style={styles.container}>
+    
       {applications.map((application) => (
-        <View key={application.id} style={styles.taskCard}>
-          {/* still handling ts */}
-          <Text style={styles.title}>{application.task.title}</Text>
-          <Text style={styles.description}> {application.task.description}</Text>
+        <View key={application.id} >
+       
+          {/* <Pressable
+            onPress={() =>
+              navigation.navigate("ProDetails", { taskId: application.task.id })
+            }
+          > */}
+          <TaskCard
+            task={application.task}
+            {...{ onApply: null, onCancel: null, onToggleLike: null }}
+          />
+          {/* </Pressable> */}
+        
 
-          {application.status === "Pending" ? (
-            <MaterialIcons name="access-time-filled" size={24} color="black" />
-          ) : application.status === "Accepted" ? (
-            <MaterialIcons name="verified" size={24} color="green" />
-          ) : application.status === "Rejected" ? (
-            <MaterialIcons name="cancel" size={24} color="red" />
-          ) : null}
         </View>
       ))}
+     
     </ScrollView>
   );
 };
 const styles = StyleSheet.create({
-  taskCard: {
-    backgroundColor: "#FFFFFF",
-    borderRadius: 20,
-    padding: 16,
-    marginBottom: 16,
-    elevation: 8,
-  },
-  title: {
-    fontSize: 16,
-    fontWeight: "bold",
-    marginBottom: 8,
-    color: "#333",
-  },
-  description: {
-    fontSize: 14,
-    marginLeft: 8,
+  container: {
+    paddingHorizontal: 18,
+    paddingVertical: 9,
+    overflow: "hidden",
+    rowGap: 11,
+    backgroundColor: "#fff",
   },
 });
 

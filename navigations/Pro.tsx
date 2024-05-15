cbeb4ab1d9b60ef5aad154df036794ddda1c876f
lindamnasri <mnasrilinda20@gmail.com>
2024-05-15 20:43:24 +0100

    add header title for some screens

--- navigations/Pro.tsx
+++ navigations/Pro.tsx
@@ -16,25 +16,76 @@ const ProNavigator = ({ Stack }) => {
   return (
     <Stack.Navigator
       screenOptions={{
-        headerShown: false,
+        headerStyle: {
+          backgroundColor: "#ced5e4",
+          borderBottomColor: "#e6eaf1",
+          padding: 20,
+          borderBottomWidth: 1,
+        },
+        headerBackTitleVisible: false,
+        headerBackImage: () => (
+          <View>
+            <Text>Back</Text>
+          </View>
+        ),
+        animationEnabled: false,
       }}
       initialRouteName="task-list"
     >
-      <Stack.Screen name="AddService" component={ProIndex} />
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="MyServices" component={MyServices} />
+      <Stack.Screen
+        name="AddService"
+        component={ProIndex}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="MyServices"
+        component={MyServices}
+        options={{ title: "My Services" }}
+      />
       <Stack.Screen name="ServiceDetails" component={ServiceDetails} />
-      <Stack.Screen name="task-list" component={TaskList} />
-      <Stack.Screen name="ProfileIndex" component={Profile} />
+      <Stack.Screen
+        name="task-list"
+        component={TaskList}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="ProfileIndex"
+        component={Profile}
+        options={{ headerShown: false }}
+      />
       <Stack.Screen name="Tabs" component={Tabs} />
-      <Stack.Screen name="FavouriteTasksList" component={FavouriteTasksList} />
-      <Stack.Screen name="ProDetails" component={proDetails} />
-      <Stack.Screen name="AppliedJobs" component={AppliedTasks} />
-      <Stack.Screen name="ConversationList" component={ConversationList} />
-      <Stack.Screen name="Conversation" component={Conversation} />
+      <Stack.Screen
+        name="FavouriteTasksList"
+        component={FavouriteTasksList}
+        options={{ title: "Favorite Tasks" }}
+      />
+      <Stack.Screen
+        name="ProDetails"
+        component={proDetails}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="AppliedJobs"
+        component={AppliedTasks}
+        options={{ title: "Applications" }}
+      />
+      <Stack.Screen
+        name="ConversationList"
+        component={ConversationList}
+        options={{ title: "Messages" }}
+      />
+      <Stack.Screen
+        name="Conversation"
+        component={Conversation}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   );
 };
 
 export default ProNavigator;
-

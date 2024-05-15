916ed0ceca7adfcfbfde1a0e2a6a159588599768
lindamnasri <mnasrilinda20@gmail.com>
2024-05-15 20:46:10 +0100

    styling tweaks

--- screens/professional/MyServices.tsx
+++ screens/professional/MyServices.tsx
@@ -1,8 +1,17 @@
 import { useState, useEffect } from "react";
-import { View, Text, StyleSheet, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  ImageBackground,
+  Image
+} from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import { Service } from "../../types/Service";
 import { ApiClient } from "../../utils/api";
+
+import gradient from "../../assets/images/orange_gradient.png";
 
 const MyServices = ({ navigation }) => {
   const [services, setServices] = useState<Service[]>([]);
@@ -20,39 +29,56 @@ const MyServices = ({ navigation }) => {
   }, []);
 
   return (
-    <View>
-      <Text style={styles.title1}>My Services,</Text>
-      {services.map((service) => (
-        <Pressable
-          key={service.id}
-          onPress={() =>
-            navigation.navigate("ServiceDetails", { serviceId: service.id })
-          }
-        >
-          <View style={styles.card}>
-            <Text style={styles.title}>{service.title}</Text>
-            <View style={styles.infoSection}>
-              <View style={styles.info}>
-                <Icon name="event" size={20} color="#FF9800" />
-                <Text style={styles.infoText}>{service.availability}</Text>
-              </View>
-              <View style={styles.info}>
-                <Icon name="place" size={20} color="#4CAF50" />
-                <Text style={styles.infoText}>{service.location}</Text>
-              </View>
-              <View style={styles.info}>
-                <Icon name="attach-money" size={20} color="#F44336" />
-                <Text style={styles.infoText}>{service.price}</Text>
+    <ImageBackground
+      source={gradient}
+      imageStyle={{ opacity: 0.6 }}
+      resizeMode="cover"
+      style={styles.container}
+    >
+      <View>
+        {/* <Text style={styles.title1}>My Services,</Text> */}
+        {services.map((service) => (
+          <Pressable
+            key={service.id}
+            onPress={() =>
+              navigation.navigate("ServiceDetails", { serviceId: service.id })
+            }
+          >
+            <View style={styles.card}>
+            {/* <Image style={styles.avatar} source={{ uri: service.client?.avatar }} /> */}
+              <Text style={styles.title}>{service.title}</Text>
+              <View style={styles.infoSection}>
+                <View style={{ flexDirection: "row" }}>
+                  <View style={[styles.info, { flex: 1 }]}>
+                    <Icon name="place" size={20} color="#4CAF50" />
+                    <Text style={styles.infoText}>{service.location}</Text>
+                  </View>
+                  <View style={[styles.info, { flex: 1 }]}>
+                    <Icon name="attach-money" size={20} color="#F44336" />
+                    <Text>{service.price}</Text>
+                  </View>
+                </View>
+                <View style={styles.info}>
+                  <Icon name="event" size={20} color="#FF9800" />
+                  <Text style={styles.infoText}>{service.availability}</Text>
+                </View>
               </View>
             </View>
-          </View>
-        </Pressable>
-      ))}
-    </View>
+          </Pressable>
+        ))}
+      </View>
+    </ImageBackground>
   );
 };
 export default MyServices;
 const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    overflow: "hidden",
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: 28,
+  },
   card: {
     backgroundColor: "#FFF",
     padding: 15,
@@ -61,7 +87,7 @@ const styles = StyleSheet.create({
     marginVertical: 6,
     borderWidth: 1,
     borderColor: "#bfbfbf",
-    paddingHorizontal: 18,
+    paddingHorizontal: 25,
   },
   title: {
     fontSize: 20,
@@ -70,21 +96,24 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
   infoSection: {
-    flexDirection: "row",
-    flexWrap: "wrap",
+    // flexWrap: "wrap",
   },
   info: {
     flexDirection: "row",
     alignItems: "center",
-    marginBottom: 5,
-    flexBasis: "50%",
+    marginBottom: 10,
   },
   infoText: {
     marginLeft: 5,
   },
   title1: {
-    fontSize: 30,
+    fontSize: 32,
     fontWeight: "600",
     color: "#2e2e2e",
+  },
+  avatar: {
+    height: 45,
+    width: 45,
+    borderRadius: 45 / 2,
   },
 });
